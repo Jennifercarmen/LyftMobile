5fb968285dbce57cdbb4a5f38f4aa8f751db47ad
Jennifercarmen <jennfercarmens@gmail.com>
2017-12-30 22:42:47 -0500

    view signup

--- js/app.js
+++ js/app.js
@@ -7,10 +7,31 @@ $(document).ready(function () {
         window.location.href = 'views/signup.html';
 
     });
+// Sign up
+var $number = $('#inputNumber');
+
+var validatenumber = false;
+
+// Aqui indicar que se puede implementar la función como variable
+function activeButton() {
+  if (validatenumber) {
+    $('form > button').attr('disabled', false);
+  }
+}
+
+function desactiveButton() {
+  $('form > button').attr('disabled', 'disabled');
+} 
 
 
-
-
+$number.on('input', function(event) {
+    if ($(this).val().length === 10) {
+        validatenumber = true;
+        activeButton(); 
+      } else {
+        desactiveButton(); 
+      }
+});
 
 });
 
