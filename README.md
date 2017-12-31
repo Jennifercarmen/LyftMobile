cb0b8c59cdfa5e599c4d3f9c1b3ec294dfff973a
Jennifercarmen <jennfercarmens@gmail.com>
2017-12-30 21:10:42 -0500

    splash,home and signup creados

--- README.md
+++ README.md
@@ -3,6 +3,14 @@
 * **Unidad:** _NO REINVENTES LA RUEDA_
 
 ***
+## Objetivos
+
+-Desarrollar una web-app que replique el sitio de Lyft:
+
+
+## Desarrollado con:
+
+`HTML5` `Jquery` `CSS3` `Bootstrap`
 
 ***
 
---
+++ js/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    setTimeout(function () {
+        $(".splash").hide();
+        $(".home").show();
+    }, 3000);
+    $(".signUp").click(function () {
+        window.location.href = 'views/signup.html';
+
+    });
+
+
+
+
+
+});
+
